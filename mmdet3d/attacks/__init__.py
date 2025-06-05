a0cf11bbf6318c850f5c284630606708445f1f27
Matthew <wu.ming.shyen@gmail.com>
2025-06-05 12:47:12 +0800

    ious attack py

--- mmdet3d/attacks/__init__.py
+++ mmdet3d/attacks/__init__.py
@@ -3,9 +3,10 @@ from .PGD import PGD
 from .FGSM import FGSM
 from .AutoPGD import AutoPGD
 from .CWAttack import CWAttack
+from .IOUSAttack import IOUSAttack
 from .PatchAttack import PatchAttack
 from .base import BaseAttacker
 
 __all__ = [
-    'CWAttack', 'PGD', 'FGSM', 'AutoPGD', 'PatchAttack', 'BaseAttacker'
+    'CWAttack', 'PGD', 'FGSM', 'AutoPGD', 'IOUSAttack', 'PatchAttack', 'BaseAttacker'
 ]
