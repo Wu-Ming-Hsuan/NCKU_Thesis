67fd64de683758539fce8dc6f040ddd2e02ac387
matthew <wu.ming.shyen@gmail.com>
2025-06-25 20:49:24 +0800

    attack hook

--- mmdet3d/engine/hooks/attack_hook.py
+++ mmdet3d/engine/hooks/attack_hook.py
@@ -1,5 +1,6 @@
 # file: mmdet3d/hooks/adv_hook.py
 import torch
+import random
 from mmengine.hooks import Hook
 from mmdet3d.attacks import ATTACKS
 from mmengine.registry import HOOKS
@@ -11,6 +12,11 @@ class AttackHook(Hook):
         self.attack = ATTACKS.build(attack_cfg)
 
     def before_train_iter(self, runner, batch_idx, data_batch):
+        if random.random() <= 0.8:
+            with torch.enable_grad():
+                data_batch = self.attack.run(runner.model, data_batch, self.attack_mode)
+
+    def before_val_iter(self, runner, batch_idx, data_batch):
         with torch.enable_grad():
             data_batch = self.attack.run(runner.model, data_batch, self.attack_mode)
 
